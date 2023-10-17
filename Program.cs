aae3742479bcbea5b690ca0f0a328fd5be8e51e8
咲夜琉命 <708855691@qq.com>
2023-10-17 15:40:58 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using PSHostsFile;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickBestCloudflare
+{
+    internal class Program
+    {
+        const string urlPath = "urls.txt";
+        
+        static void Main(string[] args)
+        {
+            var urls = File.ReadAllLines(urlPath);
+
+            foreach (var url in urls)
+            {
+                var record = HostsFile.Get(url);
+                Console.WriteLine(record);
+            }
+        }
+    }
+}
